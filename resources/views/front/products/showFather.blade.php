e8af9c16c247d3189f151d3f8c68532a334e1738
Arnau Macaya <arnau.macaya@studiogenesis.es>
2020-11-19 09:53:45 +0100

    Mostrar imatge fatherCategory

--- resources/views/front/products/showFather.blade.php
+++ resources/views/front/products/showFather.blade.php
@@ -67,7 +67,7 @@
                             <div class="position-relative">
                                 <a href="{{LaravelLocalization::getURLFromRouteNameTranslated(App::getLocale(),'routes.products.show', [
                                     "productCategory" => $product,
-                                    ])}}" title="Accede a la información"><img class="w-100 border-img" src="vsdd"></a>
+                                    ])}}" title="Accede a la información"><img class="w-100 border-img" src="{{ route('carousel.getImage', str_replace("/",";",$product->primaryImage->path))}}"></a>
                                 <div class="position-absolute transform-t-50 d-flex b-0 r-0 mr-3">
                                     <a href="{{LaravelLocalization::getURLFromRouteNameTranslated(App::getLocale(),'routes.products.show', [
                                         "productCategory" => $product,
