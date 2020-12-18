c336e8fdd1677f9ff60fd26d44fafd9a704565dd
Diego Agudo <diego.agudo@studiogenesis.es>
2020-12-18 13:37:12 +0100

    provar per veure si es veu be

--- resources/views/front/home/categories.blade.php
+++ resources/views/front/home/categories.blade.php
@@ -33,7 +33,7 @@
 @else
     <a href="{{LaravelLocalization::getURLFromRouteNameTranslated(App::getLocale(),'routes.applications.show', [
                 "applicationCategory" => $homeApp->lang()->slug
-            ])}}" class="col-lg-3 col-md-4 col-sm-6 position-relative">
+            ])}}" class="col-lg-3 col-md-4 col-sm-6 position-relative {{$key >= 4 ? 'mt-3' : ''}}">
         <img class="w-100 border-img" src="{{ route('carousel.getImage', str_replace("/",";",$homeApp->list_image)) }}" alt="categoria cintas de cortina">
         <div class="card col-8 position-absolute position-center-t75 text-center p-3">
             <small>{{__('Inicio.seccion1_boton')}}</small>
