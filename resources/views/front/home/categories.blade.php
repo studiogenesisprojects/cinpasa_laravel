334619877d2b6f3b0d564deb7d97b42ea26afd0a
Diego Agudo <diego.agudo@studiogenesis.es>
2020-12-21 19:04:13 +0100

    canvi imatges front

--- resources/views/front/home/categories.blade.php
+++ resources/views/front/home/categories.blade.php
@@ -4,7 +4,7 @@
     <a href="{{LaravelLocalization::getURLFromRouteNameTranslated(App::getLocale(),'routes.products.show', [
                 "productCategory" => $homeApp,
             ])}}" class="col-lg-3 col-md-4 col-sm-6 position-relative mt-3">
-        <img class="w-100 border-img" src="{{ route('carousel.getImage', str_replace("/",";",$homeApp->image)) }}" alt="categoria cintas de cortina">
+        <img class="w-100 border-img" src="{{ Storage::url($homeApp->image) }}" alt="categoria cintas de cortina">
         <div class="card col-8 position-absolute position-center-t75 text-center p-3">
             <small>{{__('Inicio.seccion1_boton')}}</small>
             <h6>{{$homeApp->getNameAttribute()}}</h6>
@@ -14,7 +14,7 @@
     <a href="{{LaravelLocalization::getURLFromRouteNameTranslated(App::getLocale(),'routes.applications.show', [
                 "applicationCategory" => $homeApp->lang()->slug
             ])}}" class="col-lg-3 col-md-4 col-sm-6 position-relative mt-3">
-        <img class="w-100 border-img" src="{{ route('carousel.getImage', str_replace("/",";",$homeApp->list_image)) }}" alt="categoria cintas de cortina">
+        <img class="w-100 border-img" src="{{ Storage::url($homeApp->list_image) }}" alt="categoria cintas de cortina">
         <div class="card col-8 position-absolute position-center-t75 text-center p-3">
             <small>{{__('Inicio.seccion1_boton')}}</small>
             <h6>{{$homeApp->getNameAttribute()}}</h6>
