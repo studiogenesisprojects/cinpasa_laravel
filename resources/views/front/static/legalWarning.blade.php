6847314fe8e06b892b6b568c7b33c29c0a698eaf
Diego Agudo <diego.agudo@studiogenesis.es>
2020-12-22 17:40:28 +0100

    solucionar margenes textos legales

--- resources/views/front/static/legalWarning.blade.php
+++ resources/views/front/static/legalWarning.blade.php
@@ -1,8 +1,10 @@
 @extends('front.common.main')
 
 @section('content')
+<div class="container">
 <div class="row justify-content-center mt-5">
     <h2 class="before-title-center">{{__('Textos_legals.legal_warning_titulo')}}</h2>
 </div>
 <p class="mt-3 color-blue mb-5">{{__('Textos_legals.legal_warning')}}</p>
+</div>
 @endsection
--- resources/views/front/static/privacyPolicy.blade.php
+++ resources/views/front/static/privacyPolicy.blade.php
@@ -1,8 +1,10 @@
 @extends('front.common.main')
 
 @section('content')
-<div class="row justify-content-center mt-5">
-    <h2 class="before-title-center">{{__('Textos_legals.privacy_polity_titulo')}}</h2>
+<div class="container">
+    <div class="row justify-content-center mt-5">
+        <h2 class="before-title-center">{{__('Textos_legals.privacy_polity_titulo')}}</h2>
+    </div>
+    <p class="mt-3 color-blue mb-5">{{__('Textos_legals.privacy_polity')}}</p>
 </div>
-<p class="mt-3 color-blue mb-5">{{__('Textos_legals.privacy_polity')}}</p>
 @endsection
