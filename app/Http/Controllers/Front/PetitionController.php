a1b065acf31589ad4796027eb7e754187b5e2116
Diego Agudo <diego.agudo@studiogenesis.es>
2021-01-20 16:26:54 +0100

    canviar correu al que s'envien els correus

--- app/Http/Controllers/Front/PetitionController.php
+++ app/Http/Controllers/Front/PetitionController.php
@@ -62,7 +62,7 @@ class PetitionController extends Controller
             }
         }
 
-        $email_catalan_spanish = "marketing@cinpasa.com";
+        $email_catalan_spanish = "no-reply@cinpasa.es";
         // $email_others = "diego.agudoal@gmail.com";
         // $locale = $request->locale;
         // if ($locale == "es" || $locale == "ca") {
