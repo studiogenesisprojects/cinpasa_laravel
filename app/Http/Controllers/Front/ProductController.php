edf64e2448cf8e32c421552410044f1dfbde530c
Diego Agudo <diego.agudo@studiogenesis.es>
2021-01-27 17:42:20 +0100

    canviar ordre dels productes

--- app/Http/Controllers/Front/ProductController.php
+++ app/Http/Controllers/Front/ProductController.php
@@ -52,7 +52,7 @@ class ProductController extends Controller
             } elseif($filter == 2) {
                 $products = $products->sortByDesc('name');
             } else {
-                $products = $products->sortBy('order');
+                $products = $products->sortByDesc('order');
             }
             $productCategoryChildrens = $productCategory->childrens;
             $fathers = ProductCategory::where('sup_product_category', null)->get();
@@ -64,7 +64,7 @@ class ProductController extends Controller
         } elseif($filter == 2) {
             $products = $products->sortByDesc('name');
         } else {
-            $products = $products->sortBy('order');
+            $products = $products->sortByDesc('order');
         }
         return view('front.products.show', compact('productCategory', 'products','filter','more_info_trigger'));
     }
