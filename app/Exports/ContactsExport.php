4930ef70e26e07421cf2620b5fda0d696298d739
Diego Agudo <diego.agudo@studiogenesis.es>
2021-03-02 15:37:41 +0100

    autosize excel

--- app/Exports/ContactsExport.php
+++ app/Exports/ContactsExport.php
@@ -4,8 +4,9 @@ namespace App\Exports;
 
 use App\Models\Petition;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 
-class ContactsExport implements FromCollection
+class ContactsExport implements FromCollection, ShouldAutoSize
 {
     /**
      * @return \Illuminate\Support\Collection
