8b4f3933ca8ab63dc523926cc317840968327fb6
enrique vega <enricvega@studiogenesis.es>
2021-05-14 16:41:29 +0200

    ajustes rendimiento

--- resources/views/front/home/cin-lines.blade.php
+++ resources/views/front/home/cin-lines.blade.php
@@ -3,13 +3,13 @@
 <div class="col-lg-4 col-sm-6 px-0 border-card">
     <a href="{{LaravelLocalization::getURLFromRouteNameTranslated(App::getLocale(),'routes.lab.show_products',
         ["slug" => $lab->slug
-        ])}}" class="small color-black">
+        ])}}" class="small color-black" title="{{$lab->name}}">
     <img class="w-100" src="{{ Storage::url($lab->image) }}" alt="image from {{$lab->name}}">
     <div class="d-flex justify-content-between align-items-center py-4 px-3">
         <p class="font-bold color-black">{{$lab->name}}</p>
         <a href="{{LaravelLocalization::getURLFromRouteNameTranslated(App::getLocale(),'routes.lab.show_products',
         ["slug" => $lab->slug
-        ])}}" class="small color-black">{{__('Inicio.ver-productos')}}<img class="ml-3" src="{{ asset('front/img/icon-arrow-right-black.svg') }}" alt="icono flecha derecha"></a>
+        ])}}" class="small color-black" title="{{__('Inicio.ver-productos')}} {{$lab->name}}">{{__('Inicio.ver-productos')}}<img class="ml-3" src="{{ asset('front/img/icon-arrow-right-black.svg') }}" alt="icono flecha derecha"></a>
     </div>
 </a>
 </div>
