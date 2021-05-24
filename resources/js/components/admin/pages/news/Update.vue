407b59f2ea90c21c1fc82a9995ac084c555f4a39
alejandro largo <alejandro.largo@studiogenesis.es>
2021-05-24 13:07:31 +0200

    arreglo slug ruso

--- resources/js/components/admin/pages/news/Update.vue
+++ resources/js/components/admin/pages/news/Update.vue
@@ -352,7 +352,7 @@ export default {
       }
 
       str = str
-        .replace(/[^a-z0-9 -]/g, "") // remove invalid chars
+        .replace(/[^a-z0-9wа-я -]/g, "") // remove invalid chars
         .replace(/\s+/g, "-") // collapse whitespace and replace by -
         .replace(/-+/g, "-"); // collapse dashes
 
