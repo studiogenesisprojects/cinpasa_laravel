3e9d1aebcbc0964bf4d64ce47b30570437077d5c
Studiogenesis <info@studiogenesis.es>
2022-06-16 09:48:57 +0200

    SGSD-5973: commit uncommited server changes before starting the ticket

--- resources/views/mails/product_form.blade.php
+++ resources/views/mails/product_form.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/1999/REC-html401-19991224/strict.dtd">
 <html>
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
             <link href="https://fonts.googleapis.com/css?family=DM+Serif+Display|Lato:300,400,700&display=swap" rel="stylesheet">
             <title>{{ env('APP_NAME') }}</title>
