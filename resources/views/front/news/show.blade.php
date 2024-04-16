8c275f84f4180ed30a91f86460de1f771342c79a
Sergi <sergi.pinyol@studiogenesis.es>
2024-04-16 14:56:37 +0200

    SGSD-6847

--- resources/views/front/news/show.blade.php
+++ resources/views/front/news/show.blade.php
@@ -75,7 +75,9 @@
                     <h5 class="color-black font-bold mt-4">{{__('Noticias.noticias-relacionadas')}}</h5>
                     <div class="d-flex flex-row flex-wrap mt-3">
                         @foreach ($news->relatedNews as $related)
-                        <a href="{{LaravelLocalization::getURLFromRouteNameTranslated(App::getLocale(), 'routes.news.show', ["news" => $related])}}" class="btn-noticia mt-3 px-3 py-2 ml-1">{{$related->lang(App::getLocale())->title}}</a>
+                        @if($related->active)
+                            <a href="{{LaravelLocalization::getURLFromRouteNameTranslated(App::getLocale(), 'routes.news.show', ["news" => $related])}}" class="btn-noticia mt-3 px-3 py-2 ml-1">{{$related->lang(App::getLocale())->title}}</a>
+                        @endif
                         @endforeach
                     </div>
                 @endif
