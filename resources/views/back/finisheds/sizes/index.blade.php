88e2894d86b215f20cda30020a94fb2ac24e092e
Eduard Altes <eduard.altes@studiogenesis.es>
2025-07-01 12:41:46 +0200

    fix: fixed local windsurf unlisted files for prevoius fix

--- resources/views/back/finisheds/sizes/index.blade.php
+++ resources/views/back/finisheds/sizes/index.blade.php
@@ -33,7 +33,7 @@
                                         <div class="btn-group">
                                             <button aria-expanded="false" aria-haspopup="true" class="btn btn-default dropdown-toggle" data-toggle="dropdown" id="dropdownMenuButton2" type="button"><i class="icon-options-vertical"></i></button>
                                             <div aria-labelledby="dropdownMenuButton2" class="dropdown-menu dropdown-menu-right">
-                                                @if(Auth()->user()->role->canUpdate(App\Models\Section::find(config('app.enabled_sections.acabados'))))
+                                                @if(Auth()->user()->role->canWrite(App\Models\Section::find(config('app.enabled_sections.acabados'))))
                                                     <a href="{{ route('tamanos.edit', $size->id)}}" class="dropdown-item"><i class="ti-pencil"></i> Editar</a>
                                                 @endif
                                                 @if(Auth()->user()->role->canDelete(App\Models\Section::find(config('app.enabled_sections.acabados'))))
